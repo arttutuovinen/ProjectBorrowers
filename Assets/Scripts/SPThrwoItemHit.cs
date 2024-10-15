9f5f8e9b421fbcfa80f380d335ec718533774ce0
Ardnz <tuovinen.arttu@gmail.com>
2024-10-15 21:35:52 +0300

    Updated SPThrowItemHit

--- Assets/Scripts/SPThrwoItemHit.cs
+++ Assets/Scripts/SPThrwoItemHit.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class SPThrwoItemHit : MonoBehaviour
 {
-    public float knockbackForce = 5f;  // Strength of the knockback in the random direction
-    public float upwardForce = 20f;  // Strength of the upward force
+    public float knockbackForce = 10f;  // Strength of the knockback in the random direction
+    public float upwardForce = 5f;  // Strength of the upward force
     public float knockbackDuration = 0.2f;
     private bool isKnockedBack = false;
     private SmallPlayerMovement playerMovement;
     private float destroyTime = 2f;
 
-    private void Start()
+    void Start()
     {
-        // Get the reference to the SmallPlayerMovement script attached to the player
         playerMovement = GetComponent<SmallPlayerMovement>();
     }
 
@@ -22,6 +21,7 @@ public class SPThrwoItemHit : MonoBehaviour
         // Check if the player got hit by an object tagged "BPThrowItemWeapon"
         if (other.gameObject.CompareTag("BPThrowItemWeapon"))
         {
+            Debug.Log("SP was HIT");
             // Calculate a random direction in the XZ plane (horizontal direction)
             Vector3 randomDirection = new Vector3(
                 Random.Range(-1f, 1f),
@@ -29,16 +29,12 @@ public class SPThrwoItemHit : MonoBehaviour
                 Random.Range(-1f, 1f)
             ).normalized;
 
-            // Add the upward force to the direction
-            Vector3 knockbackDirection = randomDirection + Vector3.up * upwardForce;
-            // Start the knockback coroutine
+            Vector3 knockbackDirection = randomDirection * knockbackForce + Vector3.up * upwardForce;
             StartCoroutine(KnockbackCoroutine(knockbackDirection));
-
-            // Disable player movement when hit by a throw item weapon
             playerMovement.DisableMovement();
 
             // After 2 seconds, re-enable movement (for example)
-            Invoke("ReEnableMovement", 3f);
+            Invoke("ReEnableMovement", knockbackDuration + 3f);
             Destroy(other, destroyTime);
         }
     }
@@ -61,6 +57,8 @@ public class SPThrwoItemHit : MonoBehaviour
             timer += Time.deltaTime;
             yield return null;
         }
+        // Reset the knockback state after the duration has ended
+        isKnockedBack = false;
     }
     private void ReEnableMovement()
     {
