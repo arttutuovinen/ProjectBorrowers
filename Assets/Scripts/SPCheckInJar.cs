258ff212a141b894034f3bdbe99c4d6b401c0033
Ardnz <tuovinen.arttu@gmail.com>
2025-09-28 20:02:44 +0300

    Updated BPPoints. Started updating SPPoints

--- Assets/Scripts/SPCheckInJar.cs
+++ Assets/Scripts/SPCheckInJar.cs
@@ -6,28 +6,33 @@ using TMPro;
 
 public class SPCheckInJar : MonoBehaviour
 {
-    public TextMeshProUGUI revealText;
+    public TextMeshProUGUI scoreText;
     public string playerTag = "SmallPlayer"; 
-    public float resetDelay = 5f;
+    public float resetDelay = 3f;
+    public GameObject smallPlayer;
+    public GameObject finish;
+    private int score = 0;
 
     private void Start()
-    {   
-            revealText.gameObject.SetActive(false);
+    {
+        scoreText.text = score.ToString();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
-            revealText.gameObject.SetActive(true);
-            StartCoroutine(RestartSceneAfterDelay());
+            // Add 1 point to the score
+            score++;
+            scoreText.text = score.ToString();
+            // Start teleport coroutine
+            StartCoroutine(TeleportAfterDelay());
         }
     }
 
-    private IEnumerator RestartSceneAfterDelay()
+    private IEnumerator TeleportAfterDelay()
     {
         yield return new WaitForSeconds(resetDelay); // Wait for the specified delay
-        Scene currentScene = SceneManager.GetActiveScene(); // Get the current active scene
-        SceneManager.LoadScene(currentScene.name); // Reload the scene
+        smallPlayer.transform.position = finish.transform.position;
     }
     
 }
--- Assets/Scripts/Treasure Spawner.cs
+++ Assets/Scripts/Treasure Spawner.cs
@@ -1,27 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.SceneManagement;
 public class TreasureSpawner : MonoBehaviour
 {
-    // Array to hold all spawn points found in the scene
     private GameObject[] spawnPoints;
-
-    // Reference to the player object in the scene
     public GameObject treasure;
+    public string playerTag = "SmallPlayer";
+    public TextMeshProUGUI smallPlayerScoreText;
+    private int score = 0;
 
     void Start()
     {
         // Find all spawn points in the scene tagged as "SpawnPoint"
         spawnPoints = GameObject.FindGameObjectsWithTag("TreasureSpawnPoint");
-
-        // Check if there are any spawn points
-        if (spawnPoints.Length == 0)
-        {
-            Debug.LogError("No spawn points found in the scene!");
-            return;
-        }
-
+        smallPlayerScoreText.text = score.ToString();
         // Teleport the player to a random spawn point
         TeleportTreasureToRandomPoint();
     }
@@ -29,18 +23,29 @@ public class TreasureSpawner : MonoBehaviour
     // Method to teleport the player to a random spawn point
     void TeleportTreasureToRandomPoint()
     {
-        if (treasure == null)
-        {
-            Debug.LogError("Treasure object not set!");
-            return;
-        }
 
         // Choose a random spawn point from the array
         int randomIndex = Random.Range(0, spawnPoints.Length);
         GameObject randomSpawnPoint = spawnPoints[randomIndex];
-
-        // Move the player to the chosen spawn point's position and rotation
         treasure.transform.position = randomSpawnPoint.transform.position;
         treasure.transform.rotation = randomSpawnPoint.transform.rotation;
+        if (!treasure.activeSelf) // reactivate if it was turned off
+        treasure.SetActive(true);
+        
     }
+
+    // Called when the CharacterController collides with another object
+    private void OnTriggerEnter(Collider hit)
+    {
+        // Check if the player has collided with the object that destroys them and if they've collected the treasure
+        if (hit.gameObject.CompareTag(playerTag) && hit.gameObject.GetComponent<SmallPlayerMovement>().isTreasureCollected == true)
+        {
+            score++;
+            smallPlayerScoreText.text = score.ToString();
+            hit.gameObject.GetComponent<SmallPlayerMovement>().isTreasureCollected = false;
+            treasure.SetActive(false);
+            TeleportTreasureToRandomPoint();
+        }
+    }
+    
 }
